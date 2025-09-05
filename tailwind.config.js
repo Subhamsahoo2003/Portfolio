cfeff61b06fdc63aee6ca53209fff5cb04fa62d6
subham sahoo <subhamsahoo.nfs222@gamil.com>
2025-09-05 22:55:51 +0530

    complete of first page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
  /** @type {import('tailwindcss').Config} */
 export default {
-   content: ["./src/**/*.{html,js}"],
+   content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}",
+    "./public/index.html",
+   ],
    theme: {
      extend: {
-        fontFamily: {
-          google: ['"Pirata One"', 'system-ui'],
-     },
+      fontFamily: {
+        google: ['"Pirata One"', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+      },
    },
   },
    plugins: [],
